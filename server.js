b1f3f5db471b899974fd51adb7cc43001249118d
kowalkem <kowalkem@vp.pl>
2020-01-19 05:42:22 +0100

    Fix conditions for different responses

--- server.js
+++ server.js
@@ -8,17 +8,19 @@ server.use(express.static("public"));
 
 server.get("/", (req, res) => res.sendFile(__dirname + "views/index.html"));
 server.get("/api/hello", (req, res) => res.json({greeting: "Hello there"}));
-server.get("/api/timestamp/:date_string", (req, res) => {
+server.get("/api/timestamp/:date_string?", (req, res) => {
     const dateString = req.params.date_string;
     let dateInput;
-    if (dateString.indexOf("-") >= 0) {
-        dateInput = new Date(dateString);
-    } else if (dateString === "") {
-        dateInput = new Date();
+    if (dateString) {
+        if (dateString.indexOf("-") >= 0) {
+            dateInput = new Date(dateString);
+        } else {
+            dateInput = new Date(Number(dateString));
+        }
     } else {
-        dateInput = new Date(Number(dateString));
+        dateInput = new Date();
     }
-    if (dateInput.getTime() === null) {
+    if (dateInput.toUTCString() === "Invalid Date") {
         return res.json({error: "Invalid Date"});
     }
     return res.json({unix: dateInput.getTime(), utc: dateInput.toUTCString()});
