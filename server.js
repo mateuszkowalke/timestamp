bb690114a0f5d17c52be057661d8c1a55e7cd38c
kowalkem <kowalkem@vp.pl>
2020-01-19 05:27:45 +0100

    Add /api/timestamp get handler

--- server.js
+++ server.js
@@ -8,5 +8,20 @@ server.use(express.static("public"));
 
 server.get("/", (req, res) => res.sendFile(__dirname + "views/index.html"));
 server.get("/api/hello", (req, res) => res.json({greeting: "Hello there"}));
+server.get("/api/timestamp/:date_string", (req, res) => {
+    const dateString = req.params.date_string;
+    let dateInput;
+    if (dateString.indexOf("-") >= 0) {
+        dateInput = new Date(dateString);
+    } else if (dateString !== "") {
+        dateInput = new Date();
+    } else {
+        dateInput = new Date(Number(dateString));
+    }
+    if (dateInput.getTime() === null) {
+        return res.json({error: "Invalid Date"});
+    }
+    return res.json({unix: dateInput.getTime(), utc: dateInput.toUTCString()});
+});
 
 server.listen(3000, () => console.log("Listening on port 3000"));
